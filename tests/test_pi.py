15da408ae40a010525c826b43ff447b985689bdb
Rick Kawala <rkawala@gmail.com>
2021-05-28 11:48:52 -0700

    Better import statement. Also, expected and actual values were reversed.

--- tests/test_pi.py
+++ tests/test_pi.py
@@ -1,5 +1,5 @@
-from doodles import pi
+from doodles.pi import do_iterate
 from .test_helpers import *
 
 def test_three_iterations():
-    assert_almost_equals(pi.do_iterate(300), 3.13, 0.01)
+    assert_almost_equals(3.13, do_iterate(300), 0.01)
