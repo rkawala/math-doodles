a9eda431ad677f191bb4e09d51c7c67cec0e15f5
Rick Kawala <rkawala@gmail.com>
2021-09-17 19:22:33 -0700

    Complete doodle for binary guessing cards.

--- doodles/binary_guessing_cards.py
+++ doodles/binary_guessing_cards.py
@@ -1,6 +1,35 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
+#
+# My nephew showed me that there was a number guessing technique based on binary math. This code prints the cards
+# that let you do the trick. Or https://web.northeastern.edu/seigen/11Magic/Binary/magic_trick_instr.pdf is a
+# write-up of how it works. If that link dies, try googling "binary card trick".
+import math
+import sys
+
+
+def range_inclusive(start, end):
+    return range(start, end + 1)
 
 
 def power_of_two_generator(final_exponent):
-    return (1 << p for p in range(0, final_exponent + 1))
+    return (1 << p for p in range_inclusive(0, final_exponent))
+
+
+def numbers_with_bit_set(power_of_two, max_number):
+    return [i for i in range_inclusive(1, max_number) if i & power_of_two]
+
+
+def generate_cards(max_number):
+    card_count = int(math.log2(math.ceil(max_number)))
+    return [[card_number, numbers_with_bit_set(card_number, max_number)]
+            for card_number in power_of_two_generator(card_count)]
+
+def main(args):
+    if len(args) != 2:
+        exit(f"Usage: {args[0]} one-to-this-value")
+    for card in generate_cards(int(args[1])):
+        print(card)
+
+if __name__ == '__main__':
+    main(sys.argv)
--- tests/test_binary_guessing_cards.py
+++ tests/test_binary_guessing_cards.py
@@ -1,7 +1,44 @@
+from io import StringIO
+from unittest import mock
+
 from doodles.binary_guessing_cards import *
 from truth.truth import AssertThat
+from nose.tools import assert_raises
 
 def test_powers_of_two_generator():
+    AssertThat(list(power_of_two_generator(0))).ContainsExactly(1).InOrder()
+    AssertThat(list(power_of_two_generator(1))).ContainsExactly(1, 2).InOrder()
     AssertThat(list(power_of_two_generator(4))).ContainsExactly(1, 2, 4, 8, 16).InOrder()
 
-# Test that main works
+def test_numbers_with_bit_set_generator():
+    AssertThat(list(numbers_with_bit_set(4, 9))).ContainsExactly(4, 5, 6, 7).InOrder()
+    AssertThat(list(numbers_with_bit_set(8, 8))).ContainsExactly(8).InOrder()
+    AssertThat(list(numbers_with_bit_set(8, 9))).ContainsExactly(8, 9).InOrder()
+
+def test_generate_cards():
+    AssertThat(list(generate_cards(7))).ContainsExactly(
+        [1, [1, 3, 5, 7]],
+        [2, [2, 3, 6, 7]],
+        [4, [4, 5, 6, 7]]
+    ).InOrder()
+
+def test_exit_if_no_argument():
+    with assert_raises(SystemExit) as exc:
+        main(["wxyz"])
+    AssertThat(exc.exception.code).IsEqualTo("Usage: wxyz one-to-this-value")
+
+@mock.patch('doodles.binary_guessing_cards.generate_cards')
+def test_main_passes_args(mock_generate_cards):
+    with mock.patch('sys.stdout', new=StringIO()) as captured_output:
+        main([ "wxyz", "11" ])
+    mock_generate_cards.assert_called_with(11)
+
+def test_main_prints_cards():
+    with mock.patch('sys.stdout', new=StringIO()) as captured_output:
+        main([ "wxyz", "11" ])
+    AssertThat(captured_output.getvalue()).IsEqualTo("""\
+[1, [1, 3, 5, 7, 9, 11]]
+[2, [2, 3, 6, 7, 10, 11]]
+[4, [4, 5, 6, 7]]
+[8, [8, 9, 10, 11]]
+""")
