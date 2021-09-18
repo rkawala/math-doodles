3989dee0c0736d1f311bc769145d9dbfb416d9d6
Rick Kawala <rkawala@gmail.com>
2021-09-17 19:58:00 -0700

    I should've been starting Python with /usr/bin/env for portability.

--- doodles/binary_guessing_cards.py
+++ doodles/binary_guessing_cards.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # My nephew showed me that there was a number guessing technique based on binary math. This code prints the cards
